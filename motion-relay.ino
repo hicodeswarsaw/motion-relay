f814b38ac389f3eb36d1b7ba55880ab9f4c1986a
Joanna Skorupska <j.skorupska@icloud.com>
2017-03-14 20:23:43 +0100

    Update motion-relay.ino

--- motion-relay.ino
+++ motion-relay.ino
@@ -1,9 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+const int echopin = 6; // echo pin from distance sensor
+const int trigpin = 5; // Trigger pin from distance sensor 
 
+const int maximumRange = 40;  // Maximum measured range in CM you can change it for own purpose
+
+long duration;
+int distanceCM;
+
+void setup() 
+
+{
+  
+  Serial.begin (9600);        // just in case if you want to use serial monitor
+  pinMode (trigpin, OUTPUT);
+  digitalWrite(trigpin,LOW);
+  pinMode (echopin, INPUT );   
+  pinMode (9,OUTPUT);          // define relay pin on your arduino
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
 
+{
+  
+  digitalWrite(trigpin, HIGH);   
+  delayMicroseconds(2);
+  digitalWrite(trigpin, LOW);
+  delayMicroseconds(10);    
+  duration = pulseIn (echopin, HIGH, maximumRange*40);     
+
+  distanceCM = duration/5;
+  
+  delay (50);
+  Serial.println(distanceCM);    // Turn on the output iff distance 60 to 80 cm inclusive.
+  digitalWrite(9, distanceCM >= 5 && distanceCM <= 10);
+  
 }
+
